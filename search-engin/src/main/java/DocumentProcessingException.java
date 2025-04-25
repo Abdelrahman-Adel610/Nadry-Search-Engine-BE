739a26b494fd675e5173fd10ab065a7ad2f56805
Salah3060 <salahmostafa3060@gmail.com>
2025-04-25 23:27:11 +0300

    Indexer

---
+++ search-engin/src/main/java/DocumentProcessingException.java
@@ -0,0 +1,9 @@
+
+
+public class DocumentProcessingException  extends Exception {
+    private static final long serialVersionUID = 1L;
+
+	public DocumentProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ search-engin/src/main/java/ProcessedDocument.java
@@ -0,0 +1,64 @@
+
+
+import java.util.Set;
+
+/**
+ * Represents a processed document ready for indexing
+ */
+public class ProcessedDocument {
+    private final String docId;
+    private final String url;
+    private final String title;
+    private final String description;
+    private final String content;
+    private final String filePath;
+    private final Set<String> links;
+
+
+    public ProcessedDocument(String docId, String url, String title, 
+                           String description, String content, 
+                           String filePath, Set<String> links 
+                           ) {
+        this.docId = docId;
+        this.url = url;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+        this.filePath = filePath;
+        this.links=links;
+    }
+
+    // Getters
+    public String getDocId() {
+        return docId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public Set<String> getLinks() {
+        return links;
+    }
+    
+
+  
+
+}
