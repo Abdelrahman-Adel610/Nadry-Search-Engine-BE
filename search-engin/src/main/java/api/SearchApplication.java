a03d6addca3fc371ef1d20f341c405beefba0524
Salah3060 <salahmostafa3060@gmail.com>
2025-04-29 19:08:51 +0300

    test spring server

---
+++ search-engin/src/main/java/api/SearchApplication.java
@@ -0,0 +1,10 @@
+package api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
