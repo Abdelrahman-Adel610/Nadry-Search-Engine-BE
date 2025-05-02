db02356aaa9a5cf64a129d264596ff734039aacf
Abdelrahman Adel <engba80818233@gmail.com>
2025-05-02 16:02:35 +0300

    fix(merge)

--- search-engin/src/main/Search_Controller/supabase.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = "https://jfznfxwatpxwqoasszgh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmem5meHdhdHB4d3FvYXNzemdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4Mzg1NzIsImV4cCI6MjA2MTQxNDU3Mn0._8mpyWZ43SQcgFV6LLtxQpdVLfbqEQ2sf0XcYpeU6sw";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = { supabase };
