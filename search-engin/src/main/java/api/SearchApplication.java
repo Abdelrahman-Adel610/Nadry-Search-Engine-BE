7ab6ec731ba57162dee8c5be2c62389dc0b1e3b7
ZiadMontaser <ziadmontaser2005@gmail.com>
2025-05-04 16:15:10 +0300

    feat(search): update main class and implement search endpoint

--- search-engin/src/main/java/api/SearchApplication.java
+++ search-engin/src/main/java/api/SearchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class SearchApplication {
     public static void main(String[] args) {
+        System.out.println("Starting Search Application...");
         SpringApplication.run(SearchApplication.class, args);
     }
 }
--- search-engin/src/main/java/api/SearchController.java
+++ search-engin/src/main/java/api/SearchController.java
@@ -1,5 +1,10 @@
 package api;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SearchController {
@@ -7,4 +12,23 @@ public class SearchController {
     public String test() {
         return "Test endpoint working!";
     }
+
+    // @GetMapping("/search")
+    // public String search() {
+
+    //     SearchWrapper searchWrapper = new SearchWrapper();
+    //     // searchWrapper.setSearchType("test");
+    //     return "Search endpoint working!";
+    // }
+
+    @GetMapping("/search")
+    public List<Map<String, Object>> search(@RequestParam String query) {
+        SearchWrapper searchWrapper = new SearchWrapper();
+        searchWrapper.search(query); // or call your actual search method
+    
+        //  Map<String, Object> result = new HashMap<>();
+        List<Map<String, Object>> results = new ArrayList<Map<String,Object>>();
+         return searchWrapper.search(query);
+    }
+
 }
